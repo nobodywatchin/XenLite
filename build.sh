513404039f6576a092aa77a61db1926b8fda43c2
nobody's watching <118339799+nobodywatchin@users.noreply.github.com>
2025-01-06 22:08:02 -0800

    add tailscale and notes to build.sh
    more packages needed soon

--- build.sh
+++ build.sh
@@ -2,19 +2,34 @@
 
 set -ouex pipefail
 
-### Install packages
+# See https://github.com/centos-workstation/achillobator/issues/3
+mkdir -m 0700 -p /var/roothome
+# Fast track https://gitlab.com/fedora/bootc/base-images/-/merge_requests/71
+ln -sf /run /var/run
+# Required for Logically Bound images, see https://gitlab.com/fedora/bootc/examples/-/tree/main/logically-bound-images/usr/share/containers/systemd
+ln -sr /etc/containers/systemd/*.container /usr/lib/bootc/bound-images.d/
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# Packages
 
-# this installs a package from fedora repos
-dnf install -y tmux 
+dnf install -y avahi cockpit cockpit-machines cockpit-files libvirt tmux vim firewalld
 
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
+# NOTE: STILL MISSING XEN TOOLS!
 
-#### Example for enabling a System Unit File
+# DOCKER IS COMMENTED OUT, NEED TO STRIP THE IMAGE DOWN TOO
 
-systemctl enable podman.socket
+# Docker install: https://docs.docker.com/engine/install/centos/#install-using-the-repository
+# dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+# dnf install -y docker-ce docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Tailscale
+dnf config-manager --add-repo https://pkgs.tailscale.com/stable/centos/9/tailscale.repo
+dnf config-manager --set-disabled tailscale-stable
+dnf -y --enablerepo tailscale-stable install \
+  tailscale
+
+# Services
+
+systemctl enable cockpit.socket
+systemctl enable rpm-ostreed-automatic.timer 
+systemctl enable tailscaled.service
+systemctl disable auditd.service
